77afde802c72cb0131a909ca666ca707fb574ee8
Md Adeem <mdadeem2003@gmail.com>
2026-03-03 01:33:50 +0530

    Adding a scroll to top component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,12 +5,14 @@ import { Toaster } from 'react-hot-toast';
 import App from './App';
 import { AuthProvider } from './features/auth/context/AuthContext';
 import ErrorBoundary from './shared/components/ErrorBoundary';
+import ScrollToTop from './shared/components/ScrollToTop';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
       <BrowserRouter>
+        <ScrollToTop />
         <AuthProvider>
           <App />
           <Toaster
